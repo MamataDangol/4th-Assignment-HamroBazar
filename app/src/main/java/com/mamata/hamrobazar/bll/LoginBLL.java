39f09672371cbbb786735f0e88095a1015381cfd
MamataDangol <Mamatadangol3@gmail.com>
2020-01-10 13:27:52 +0545

    Login Bll added.

---
+++ app/src/main/java/com/mamata/hamrobazar/bll/LoginBLL.java
@@ -0,0 +1,37 @@
+package com.mamata.hamrobazar.bll;
+
+import com.deepak.hamrobazzar.api.UsersAPI;
+import com.deepak.hamrobazzar.serverresponse.SignUpResponse;
+import com.deepak.hamrobazzar.url.Url;
+import com.mamata.hamrobazar.api.UsersAPI;
+
+import java.io.IOException;
+
+import retrofit2.Call;
+import retrofit2.Response;
+import retrofit2.http.Url;
+
+public class LoginBLL {
+
+    boolean isSuccess = false;
+
+    public boolean checkUser(String email, String password) {
+
+        UsersAPI usersAPI = Url.getInstance().create(UsersAPI.class);
+        Call<SignUpResponse> usersCall = usersAPI.checkUser(email, password);
+
+        try {
+            Response<SignUpResponse> loginResponse = usersCall.execute();
+            if (loginResponse.isSuccessful() &&
+                    loginResponse.body().getStatus().equals("Login success!")) {
+
+                Url.token += loginResponse.body().getToken();
+                // Url.Cookie = imageResponseResponse.headers().get("Set-Cookie");
+                isSuccess = true;
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return isSuccess;
+    }
+}
